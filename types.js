28bca6ba319060aa583fd9ba6e17076edaaafd89
jessironcaroli-ux <jessironcaroli@gmail.com>
2026-01-02 11:49:24 -0300

    Update types.js

--- types.js
+++ types.js
@@ -1,10 +1,18 @@
 
+export const ExpenseType = {
+  FIXED: 'FIXED',
+  VARIABLE: 'VARIABLE'
+};
+
 export const UserRole = {
   ADMIN: 'ADMIN',
   CLIENT: 'CLIENT'
 };
 
-export const ExpenseType = {
-  FIXED: 'FIXED',
-  VARIABLE: 'VARIABLE'
+export const InvestmentType = {
+  MACHINERY: 'MACHINERY',
+  VEHICLE: 'VEHICLE',
+  STOCK: 'STOCK',
+  BOND: 'BOND',
+  REAL_ESTATE: 'REAL_ESTATE'
 };
